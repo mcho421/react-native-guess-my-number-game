2e069dcea6f9fe690b5f9743a65bffd69ad3071c
Mathew Chong <mathewchong.dev@gmail.com>
2025-01-23 07:43:51 +1100

    Extract Title as custom component

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,9 +1,11 @@
 import { View, Text, StyleSheet } from "react-native";
 
+import Title from "../components/Title";
+
 function GameScreen() {
   return (
     <View style={styles.screen}>
-      <Text>Opponent's Guess</Text>
+      <Title>Opponent's Guess</Title>
       GUESS
       <View>
         <Text>Higher or lower?</Text>
@@ -20,5 +22,5 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     padding: 24,
-  }
+  },
 })
