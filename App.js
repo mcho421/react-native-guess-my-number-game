cb3c2417ae64cfe7596dc4739ae43d074b60f2f1
Mathew Chong <mathewchong.dev@gmail.com>
2025-01-23 06:54:45 +1100

    Render content within SafeAreaView

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { StyleSheet, ImageBackground } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 import StartGameScreen from "./screens/StartGameScreen";
 import GameScreen from "./screens/GameScreen";
@@ -26,7 +27,7 @@ export default function App() {
         style={styles.rootScreen}
         imageStyle={styles.backgroundImage}
       >
-        {screen}
+        <SafeAreaView style={styles.rootScreen}>{screen}</SafeAreaView>
       </ImageBackground>
     </LinearGradient>
   );
