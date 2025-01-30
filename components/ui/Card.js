aa53e649d1bc4bc493c17738ecdda80a5a1a9481
Mathew Chong <mathewchong.dev@gmail.com>
2025-01-31 06:59:26 +1100

    Use Dimensions API to add breakpoint for smaller screen

--- components/ui/Card.js
+++ components/ui/Card.js
@@ -1,4 +1,4 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Dimensions } from "react-native";
 
 import Colors from "../../constants/colors";
 
@@ -8,10 +8,12 @@ function Card({ children }) {
 
 export default Card;
 
+const deviceWidth = Dimensions.get('window').width;
+
 const styles = StyleSheet.create({
   card: {
     alignItems: "center",
-    marginTop: 100,
+    marginTop: deviceWidth < 380 ? 18 : 36,
     marginHorizontal: 24,
     padding: 16,
     backgroundColor: Colors.primary800,
